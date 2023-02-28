b106ecb68f6cecadc9f5497c10415dd9e520c249
Disha <dishajadav12402@gmail.com>
2023-02-28 11:21:56 +0530

    Updates

---
+++ precache-manifest.f913e054a76249dad06270648a15d371.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f856799b2038157ee4f7",
+    "url": "/TextUtils/static/css/main.6af3293b.chunk.css"
+  },
+  {
+    "revision": "f856799b2038157ee4f7",
+    "url": "/TextUtils/static/js/main.f856799b.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TextUtils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "eeac072fcc85ca05faa8",
+    "url": "/TextUtils/static/js/runtime~main.eeac072f.js"
+  },
+  {
+    "revision": "db7344dc930f5c343037a8a4015b5182",
+    "url": "/TextUtils/index.html"
+  }
+];
